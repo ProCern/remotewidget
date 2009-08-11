664431846ac6728de3f1c2d5deed586a471db152
Erik Cunningham <ewc@erik-cunninghams-macbook-pro.local>
2009-08-11 15:10:01 -0600

    Directory heirarchy.

---
+++ js/queue.js
@@ -0,0 +1,45 @@
+var RequestQueue = function()
+{
+  this.requests = [];
+  this.results = {};
+  this.whenAllCompleted = function() { };
+};
+
+RequestQueue.prototype.addRequest = function(method, href) {
+  this.requests.push( {
+      "method":   method,
+      "href":     href,
+      completed:  false
+  });
+};
+
+RequestQueue.prototype.dispatchAll = function() {
+  var self = this;
+  $.each(self.requests, function( i, request) {
+    console.log("dispatching request: " + request.href);
+    request.method( request.href, function (r) {
+      return function(datapoints) {
+        request.completed = true;
+        console.log(request);
+        self.results[request.href] = datapoints;
+        self.checkAndComplete();
+      };
+    }(request));
+  });
+};
+
+RequestQueue.prototype.checkAndComplete = function() {
+  if (this.allRequestsComplete()) {
+    this.whenAllCompleted(this.results);
+  }
+};
+
+RequestQueue.prototype.allRequestsComplete = function() {
+  var i = 0;
+  while ( r = this.requests[i++] ) {
+    if (r.completed === false) {
+      return false;
+    }
+  }
+  return true;
+};
---
+++ js/ssbe.js
@@ -0,0 +1,77 @@
+function findservice(service_name,callback) {
+  $.jsonp({
+    url:servicedesc, 
+    success:function(services) {
+      found_service = $.grep(services.items, function(service, i) {
+        return service.service_type === service_name;
+      })[0];
+      callback(found_service);
+    }
+  });
+}
+
+function findresource(resource_name,service, callback) {
+  $.jsonp({
+    url:found_service.href, 
+    success:function(resources) {
+      resource = $.grep(resources.items, function(resource,i) {
+        return resource.name === resource_name;
+      })[0];
+      callback(resource);
+    }
+  });
+}
+
+function getresource(url,callback){
+  $.jsonp({
+    url:url,
+    success:function(res) {
+      callback(res);
+    }
+  });
+}
+
+function graphlist(s,obs_list) {
+  var results=[];
+  var requests = 0;
+  console.log(obs_list);
+
+  $.each(obs_list, function(_,req) {
+      console.log("getting "+req.url);
+    $.jsonp({
+      url:req.url,
+      complete: function(xo,txt) {
+        console.log(txt,xo);
+      },
+      success:function(data) {
+        console.log("got " + req.url);
+        results.push({label:req.label,data:$.map(data.items,function(item) {
+            var stamp = new Date();
+            stamp.setISO8601(item.timestamp);
+            return [[stamp.getTime(),item.value]];
+          })
+        });
+        requests+=1;
+
+        if (requests == obs_list.length) {
+          console.log(results);
+          $.plot($(s),results,{xaxis:{mode:"time"}});
+        }
+      }
+    });
+  });
+}
+
+function graphflot(sel,obs_href) {
+  getresource(obs_href,function(observations) {
+    var d = [];
+    $.each(observations.items,function(j,item) {
+      var stamp = new Date();
+      stamp.setISO8601(item.timestamp);
+      d.push([stamp.getTime(),item.value]);
+      });
+    var sorted = d.sort();
+    $.plot($(sel),[sorted],{xaxis:{mode:"time"}});
+  });
+}
+
